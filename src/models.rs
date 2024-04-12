7b03d2316c548a205446eb280b7106f3ea813709
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2024-04-12 22:31:26 +0200

    add automapped maps and size optimization

--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,4 @@
 pub mod song_details_json {
-    use rust_decimal::Decimal;
-    use rust_decimal::prelude::{FromPrimitive, ToPrimitive};
     use crate::api::beat_leader::models::models::DifficultyDescription;
     use crate::api::beat_saver::models::models::{MapDetail, MapDiff};
     use crate::api::score_saber::models::models::LeaderboardInfo;
@@ -33,35 +31,25 @@ pub mod song_details_json {
             }
 
             SongDetails {
-                id: bsv_map.id.to_string(),
-                hash: bsv_map.versions.first().unwrap().hash.to_string(),
-                name: bsv_map.name.to_string(),
-                metadata: Some(MapDetailMetadata {
-                    bpm: bsv_map.metadata.bpm,
-                    duration: bsv_map.metadata.duration,
-                    level_author_name: bsv_map.metadata.levelAuthorName.to_string(),
-                    song_author_name: bsv_map.metadata.songAuthorName.to_string(),
-                    song_name: bsv_map.metadata.songName.to_string(),
-                    song_sub_name: bsv_map.metadata.songSubName.to_string(),
-                }),
+                id_int: i32::from_str_radix(&bsv_map.id, 16).unwrap() as u32,
+                hash: bsv_map.versions.first().unwrap().hash.to_lowercase(),
+                duration: bsv_map.metadata.duration as u32,
                 uploader: Some(Uploader {
                     name: bsv_map.uploader.name.clone(),
-                    id: bsv_map.uploader.id,
+                    id: bsv_map.uploader.id as u32,
                     verified: bsv_map.uploader.verifiedMapper.unwrap_or(false)
                 }),
-                uploaded_at: parse_date_rfc3339_to_timestamp(&bsv_map.uploaded) as i32,
-                tags: bsv_map.tags.clone().unwrap_or(Vec::new()),
-                bpm: bsv_map.metadata.bpm,
+                uploaded_at: parse_date_rfc3339_to_timestamp(&bsv_map.uploaded) as u32,
+                tags: bsv_map.tags.clone().map_or(Vec::new(), |tags| tags.iter().map(|tag| MapTag::from_str(tag) as i32).collect()),
                 ranked: ss_leaderboards.map_or(bsv_map.ranked, |leaderboards| leaderboards.iter().any(|leaderboard| leaderboard.ranked)),
                 qualified: ss_leaderboards.map_or(bsv_map.qualified, |leaderboards| leaderboards.iter().any(|leaderboard| leaderboard.qualified)),
                 curated: bsv_map.curatedAt.clone().map_or(false, |_| true),
                 ranked_bl: bl_map_difficulties.map_or(false, |diffs| diffs.iter().any(|diff| diff.rankedTime.map_or(false, |time| time > 0))),
                 nominated_bl: bl_map_difficulties.map_or(false, |diffs| diffs.iter().any(|diff| diff.nominatedTime.map_or(false, |time| time > 0))),
                 qualified_bl: bl_map_difficulties.map_or(false, |diffs| diffs.iter().any(|diff| diff.qualifiedTime.map_or(false, |time| time > 0))),
-                up_votes: bsv_map.stats.upvotes,
-                down_votes: bsv_map.stats.downvotes,
-                downloads: bsv_map.stats.downloads,
-                duration: bsv_map.metadata.duration,
+                up_votes: bsv_map.stats.upvotes as u32,
+                down_votes: bsv_map.stats.downvotes as u32,
+                downloads: bsv_map.stats.downloads as u32,
                 automapper: bsv_map.automapper,
                 difficulties,
             }
@@ -69,26 +57,117 @@ pub mod song_details_json {
 
         fn build_difficulty(bsv_map_diff: &MapDiff, bl_map_diff: Option<&DifficultyDescription>, ss_leaderboard: Option<&LeaderboardInfo>) -> Difficulty {
 
-            let starts = ss_leaderboard.map_or(bsv_map_diff.stars.unwrap_or(0.0), |ss| ss.stars);
-            let bl_starts = bl_map_diff.map_or(0.0, |bl| bl.stars.unwrap_or(0.0));
+            let stars = ss_leaderboard.map_or(bsv_map_diff.stars.unwrap_or(0.0), |ss| ss.stars);
+            let bl_stars = bl_map_diff.map_or(0.0, |bl| bl.stars.unwrap_or(0.0));
 
             Difficulty {
-                difficulty: bsv_map_diff.difficulty.to_string(),
-                characteristic: bsv_map_diff.characteristic.to_string(),
+                difficulty: DifficultyLabel::from_str(&bsv_map_diff.difficulty) as i32,
+                characteristic: DifficultyCharacteristic::from_str(&bsv_map_diff.characteristic) as i32,
                 label: bsv_map_diff.label.clone().unwrap_or(String::new()),
-                stars: Decimal::from_f32(starts).unwrap().round_dp(2).to_f32().unwrap_or(0.0),
-                stars_bl: Decimal::from_f32(bl_starts).unwrap().round_dp(2).to_f32().unwrap_or(0.0),
-                njs: bsv_map_diff.njs,
-                nps: bsv_map_diff.nps,
-                offset: bsv_map_diff.offset,
+                stars_t100: (stars * 100.0).round() as u32,
+                stars_bl_t100: (bl_stars * 100.0).round() as u32,
+                njs_t100: (bsv_map_diff.njs * 100.0).round() as u32,
+                nps_t100: (bsv_map_diff.nps * 100.0).round() as u32,
+                offset_t100: (bsv_map_diff.offset * 100.0).round() as i32,
                 me: bsv_map_diff.me.unwrap_or(false),
                 ne: bsv_map_diff.ne.unwrap_or(false),
                 cinema: bsv_map_diff.cinema.unwrap_or(false),
                 chroma: bsv_map_diff.chroma.unwrap_or(false),
-                bombs: bsv_map_diff.bombs,
-                notes: bsv_map_diff.notes,
-                obstacles: bsv_map_diff.obstacles,
+                bombs: bsv_map_diff.bombs as u32,
+                notes: bsv_map_diff.notes as u32,
+                obstacles: bsv_map_diff.obstacles as u32,
             }
         }
+    }
+
+    impl DifficultyLabel {
+        pub fn from_str(label: &str) -> DifficultyLabel {
+
+            let lower_label = label.to_lowercase();
+
+            match lower_label.as_str() {
+                "easy" => DifficultyLabel::Easy,
+                "normal" => DifficultyLabel::Normal,
+                "hard" => DifficultyLabel::Hard,
+                "expert" => DifficultyLabel::Expert,
+                "expertplus" => DifficultyLabel::ExpertPlus,
+                _ => DifficultyLabel::UnknownLabel
+            }
+        }
+    }
+
+    impl DifficultyCharacteristic {
+
+        pub fn from_str(label: &str) -> DifficultyCharacteristic {
+
+            let lower_label = label.to_lowercase();
+
+            match lower_label.as_str() {
+                "standard" => DifficultyCharacteristic::Standard,
+                "noarrows" => DifficultyCharacteristic::NoArrows,
+                "one saber" => DifficultyCharacteristic::OneSaber,
+                "90 degrees" => DifficultyCharacteristic::NinetyDegree,
+                "360 degrees" => DifficultyCharacteristic::ThreesixtyDegree,
+                _ => DifficultyCharacteristic::UnknownCharacteristic
+            }
+        }
+
+    }
+
+    impl MapTag {
+
+        pub fn from_str(tag: &str) -> MapTag {
+            let lower_tag = tag.to_lowercase();
+
+            match lower_tag.as_str() {
+                "dance" => MapTag::Dance,
+                "swing" => MapTag::Swing,
+                "nightcore" => MapTag::Nightcore,
+                "folk" => MapTag::Folk,
+                "family" => MapTag::Family,
+                "ambient" => MapTag::Ambient,
+                "funk" => MapTag::Funk,
+                "jazz" => MapTag::Jazz,
+                "soul" => MapTag::Soul,
+                "speedcore" => MapTag::Speedcore,
+                "punk" => MapTag::Punk,
+                "rb" => MapTag::Rb,
+                "holiday" => MapTag::Holiday,
+                "vocaloid" => MapTag::Vocaloid,
+                "j-rock" => MapTag::JRock,
+                "trance" => MapTag::Trance,
+                "drumbass" => MapTag::Drumbass,
+                "comedy" => MapTag::Comedy,
+                "instrumental" => MapTag::Instrumental,
+                "hardcore" => MapTag::Hardcore,
+                "k-pop" => MapTag::KPop,
+                "kpop" => MapTag::KPop,
+                "indie" => MapTag::Indie,
+                "techno" => MapTag::Techno,
+                "house" => MapTag::House,
+                "game" => MapTag::Game,
+                "film" => MapTag::Film,
+                "alt" => MapTag::Alt,
+                "dubstep" => MapTag::Dubstep,
+                "metal" => MapTag::Metal,
+                "anime" => MapTag::Anime,
+                "hiphop" => MapTag::Hiphop,
+                "j-pop" => MapTag::JPop,
+                "jpop" => MapTag::JPop,
+                "rock" => MapTag::Rock,
+                "pop" => MapTag::Pop,
+                "electronic" => MapTag::Electronic,
+                "classical-orchestral" => MapTag::ClassicalOrchestral,
+                "accuracy" => MapTag::Accuracy,
+                "balanced" => MapTag::Balanced,
+                "challenge" => MapTag::Challenge,
+                "dancestyle" => MapTag::Dancestyle,
+                "fitness" => MapTag::Fitness,
+                "speed" => MapTag::Speed,
+                "tech" => MapTag::Tech,
+                _ => MapTag::UnknownTag,
+            }
+        }
+
     }
 }
